ba67ee956932482bdd367547e3ef286a48f0e6bd
Khang <khang.job@gmail.com>
2021-10-27 21:08:56 +0700

    Set up default timezone and locale to Vietname

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module Tntt
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Ho_Chi_Minh"
+    config.i18n.default_locale = :vi
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
