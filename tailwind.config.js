81a379c01361c32fae5dfa0aad45a3ee33ee617a
Khang <khang.job@gmail.com>
2021-10-27 21:56:30 +0700

    Update tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   mode: 'jit',
-  purge: [],
+  purge: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/javascripts/**/*.js'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
