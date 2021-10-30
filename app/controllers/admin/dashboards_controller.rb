efac969caa39d82625aca349918ddf7929c04942
Khang <khang.job@gmail.com>
2021-10-30 11:11:00 +0700

    Fix bug: show incorrect feastday

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -2,10 +2,10 @@ class Admin::DashboardsController < AdminController
   def show
     month = Time.zone.now.month
     next_month = (Time.zone.now + 1.month).month
-    @months = Person.where(active: true)
-                    .where('extract(month from feastday) IN (?)', [month, next_month])
-                    .order(:feastday)
-                    .group_by { |person| person.feastday.month }
-                    .values
+    @months, @next_months = Person.where(active: true)
+                                  .where('extract(month from feastday) IN (?)', [month, next_month])
+                                  .order(:feastday)
+                                  .group_by { |person| person.feastday.month }
+                                  .values
   end
 end
