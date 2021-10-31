877dbb32afb3296ff8cd9ab4d6b722d468859955
Khang <khang.job@gmail.com>
2021-10-31 13:52:47 +0700

    Change people index UI

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -2,6 +2,10 @@ class Admin::PeopleController < AdminController
   def index
     @people = Person.where(active: true)
                     .eager_load(:level)
-                    .order(:feastday)
+                    .order(:created_at)
+  end
+
+  def show
+    @person = Person.where(active: true).find(params[:id])
   end
 end
