e4baca7e045b18b43e97e5cd7437e1643bdf54f4
Khang <khang.job@gmail.com>
2021-12-18 16:23:24 +0700

    Comment out the new people path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /vi|en/ do
     devise_for :users
-    resources :people, only: %i[new create]
+    # resources :people, only: %i[new create]
     root to: 'home#index'
 
     namespace :managers do
