07b4581714921188e506931d570be0759d9537ac
Khang <khang.job@gmail.com>
2022-05-02 21:30:50 +0700

    Add WEB_ACTIVE env

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,35 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /vi|en/ do
-    devise_for :users
-    # resources :people, only: %i[new create]
-    root to: 'home#index'
+  if ENV['WEB_ACTIVE'] == '1'
+    scope "(:locale)", locale: /vi|en/ do
+      devise_for :users
+      # resources :people, only: %i[new create]
+      root to: 'home#index'
 
-    namespace :managers do
-      resource :dashboard, only: :show
-      resources :people, only: %i[index show edit update create new]
-      resources :people_presences, only: %i[new create edit update]
-      resources :scores, only: %i[show]
+      namespace :managers do
+        resource :dashboard, only: :show
+        resources :people, only: %i[index show edit update create new]
+        resources :people_presences, only: %i[new create edit update]
+        resources :scores, only: %i[show]
 
-      resources :levels, only: [] do
-        scope module: :levels do
-          resources :people_presences, only: :index
+        resources :levels, only: [] do
+          scope module: :levels do
+            resources :people_presences, only: :index
+          end
         end
+
+        root to: 'dashboards#show'
       end
 
-      root to: 'dashboards#show'
-    end
-
-    namespace :admin do
-      resource :dashboard, only: :show
-      resources :people, only: [] do
-        collection do
-          post :export
+      namespace :admin do
+        resource :dashboard, only: :show
+        resources :people, only: [] do
+          collection do
+            post :export
+          end
         end
-      end
 
-      root to: 'dashboard#show'
+        root to: 'dashboard#show'
+      end
     end
   end
 end
