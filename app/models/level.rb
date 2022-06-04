a0ed3d6bc89672bd295b1b55ff7e579abc4288f0
Khang <khang.job@gmail.com>
2022-06-04 19:41:07 +0700

    Add tro_uy tro_ta to leaders

--- app/models/level.rb
+++ app/models/level.rb
@@ -8,7 +8,7 @@
 #  updated_at :datetime         not null
 #
 class Level < ApplicationRecord
-  LEADER_NAMES = %w(du_truong huynh_truong1 huynh_truong2 huynh_truong3).freeze
+  LEADER_NAMES = %w(du_truong huynh_truong1 huynh_truong2 huynh_truong3 tro_uy tro_ta).freeze
   STUDENT_NAMES = %w(chien_con1 chien_con2 chien_con3 au_nhi1 au_nhi2 au_nhi3 thieu_nhi1 thieu_nhi2 thieu_nhi3 nghia_si1 nghia_si2 nghia_si3 nghia_si4).freeze
   has_many :people
   has_many :active_people, -> { where(active: true) }, class_name: Person.to_s
