af252106762bf9c42af285a6179c45b9cb601452
Khang <khang.job@gmail.com>
2022-07-09 18:42:26 +0700

    Dashboard data

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,2 +1,9 @@
 class Admin::DashboardController < AdminController
+  def show
+    @students_count = Person.joins(:level).where(active: true).where(levels: { name: Level::STUDENT_NAMES }).count
+    @teachers_count = Person.joins(:level).where(active: true).where(levels: { name: Level::LEADER_NAMES }).count
+    @levels_count = Level.count
+    @students_level_ids = Level.where(name: Level::STUDENT_NAMES).ids
+    @teachers_level_ids = Level.where(name: Level::LEADER_NAMES).ids
+  end
 end
