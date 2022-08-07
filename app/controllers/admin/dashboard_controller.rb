e05132fa150b51a7077303347b0a87dfd3467545
khang <khang.job@gmail.com>
2022-08-07 18:50:47 +0700

    Limit current active level

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < AdminController
   def show
     @students_count = Person.joins(:level).where(active: true).where(levels: { name: Level::STUDENT_NAMES }).count
     @teachers_count = Person.joins(:level).where(active: true).where(levels: { name: Level::LEADER_NAMES }).count
-    @levels_count = Level.count
+    @levels_count = Level.joins(:people).distinct.count
     @students_level_ids = Level.where(name: Level::STUDENT_NAMES).ids
     @teachers_level_ids = Level.where(name: Level::LEADER_NAMES).ids
     @feast_day, @feast_people = Person.next_feastday_persons.first
