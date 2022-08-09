e7688241470bc5b48eed748bca6ef4147b3a1d93
khang <khang.job@gmail.com>
2022-08-09 21:26:01 +0700

    Show student list index

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -2,4 +2,9 @@ class LevelsController < ApplicationController
   def index
     @levels = Level.joins(:active_people).where(name: Level::STUDENT_NAMES).distinct.order(sort_order: :asc)
   end
+
+  def show
+    @level = Level.find(params[:id])
+    @people = Person.joins(:level).where(levels: { id: @level }).where(active: true).order_name_alphabel
+  end
 end
