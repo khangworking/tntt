156bba067a8b8b3b9ccaf01848d1fb6904a60ad4
Khang <khang.job@gmail.com>
2022-08-23 06:24:40 +0700

    Remove hardcode

--- app/controllers/api/v0/events_controller.rb
+++ app/controllers/api/v0/events_controller.rb
@@ -2,8 +2,8 @@ class Api::V0::EventsController < ApiApplicationController
   # before_action :authenticate_user!
 
   def index
-    start_date = 20.days.ago
-    end_date = 20.days.from_now
+    start_date = params[:start]
+    end_date = params[:end]
     events = Event.where('start_datetime >= :start_date AND start_datetime <= :end_date', start_date: start_date, end_date: end_date).map do |evt|
       {
         title: evt.title,
